2a5c1b1aacbc4eae129235f3461609d1145f7d3d
Sants <106097059+CardonaSantos@users.noreply.github.com>
2024-09-25 00:22:56 -0600

    Cambios...

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const port = process.env.PORT || 3000;
+
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+
+//   //CAMBIAR EL PUERTO PARA PRODUCCCION
+//   // await app.listen(3000);
+//   await app.listen(port);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
-
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
-  //CAMBIAR EL PUERTO PARA PRODUCCCION
-  // await app.listen(3000);
+  // Configuración de CORS para permitir solicitudes de tu dominio
+  app.enableCors({
+    origin: 'https://frontend-ui-so-production.up.railway.app', // Cambia esto por el dominio que necesites
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Si necesitas enviar cookies o cabeceras de autorización
+  });
+
   await app.listen(port);
 }
 bootstrap();
